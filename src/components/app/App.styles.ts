4973999e0d4f704461082a0d9f5bb56b72463c20
Davy de Haas <davy.dehaas98@gmail.com>
2020-12-07 20:40:27 +0100

    Remove reportWebVitals.ts, Move App component

---
+++ src/components/app/App.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledAppContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background-image: url(./background.svg);
+    background-color: #FFFFFF;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
