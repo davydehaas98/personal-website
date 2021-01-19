1d0e93b5ea04eb3fc9da2f65029e6f4fb75888e4
Davy de Haas <davy.dehaas98@gmail.com>
2021-01-19 17:56:12 +0100

    Update Projects

--- src/components/project-card/ProjectCard.styles.ts
+++ src/components/project-card/ProjectCard.styles.ts
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
 
-export const StyledLink = styled.a`
+export const StyledAnchor = styled.a`
 text-decoration: none;
+margin: 25px;
+border-radius: 25px;
+&:hover {
+  filter: opacity(0.5);
+}
 `
 
 export const StyledProjectCard = styled.div`
@@ -9,17 +14,18 @@ display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center;
-margin: 20px;
-padding: 20px;
-min-width: 350px;
+padding: 25px;
+min-width: 400px;
 min-height: 300px;
-border-radius: 20px;
-background-color: #000044;
-color: #ffffff;
+border-radius: 25px;
+border-style: solid;
+border-color: #000044;
+border-width: 5px;
+color: #000044;
 `
 
 export const StyledImage = styled.img.attrs(props => ({
   src: props.src ?? './assets/images/placeholder.svg'
 }))`
-width: 200px;
+max-width: 200px;
 `
--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import { StyledImage, StyledLink, StyledProjectCard } from './ProjectCard.styles'
+import { StyledImage, StyledAnchor, StyledProjectCard } from './ProjectCard.styles'
 
 const ProjectCard = (props: any): JSX.Element => {
   return (
-    <StyledLink href={props.link} target="_blank" rel="noopener noreferrer">
+    <StyledAnchor href={props.link} target="_blank" rel="noopener noreferrer">
       <StyledProjectCard>
         <StyledImage src={props.src}/>
         <h1>{props.name}</h1>
       </StyledProjectCard>
-    </StyledLink>
+    </StyledAnchor>
   )
 }
 
