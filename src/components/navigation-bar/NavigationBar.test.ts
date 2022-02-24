c08579a78b90923fdfd03a31bd2f7c3c578e9e7f
Davy <davy.dehaas98@gmail.com>
2022-02-24 18:33:08 +0100

    Update ESLint to v8

--- src/components/navigation-bar/NavigationBar.test.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import NavigationBar from './NavigationBar'
-
-test('Renders navigation bar.', () => {
-  // render(<NavigationBar />)
-  // const text = 'screen.getByText(/learn react/i)'
-  // expect(text).toBeInTheDocument()
-})
---
+++ src/components/navigation-bar/NavigationBar.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import NavigationBar from './NavigationBar'
+
+test('Renders navigation bar.', async () => {
+  render(<NavigationBar />)
+  const text = 'screen.getByText(/learn react/i)'
+  expect(text).toBeInTheDocument()
+})
