4d767eca2721fafa2665c932a5f99e9c917c8769
Davy <davy.dehaas98@gmail.com>
2022-05-10 20:51:09 +0200

    Add App.test.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -12,16 +12,14 @@ const App = (): JSX.Element => {
   return (
     <StyledWrapper>
       <NavigationBar/>
-
       <main role="main">
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/projects" element={<Projects/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/contact" element={<Contact/>}/>
         </Routes>
       </main>
-
       <Footer/>
     </StyledWrapper>
   )
